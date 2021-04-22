75e988f2c3b924dd0500cd53511af44db3f66a4f
tamierlan <hudojnik.1993@gmail.com>
2021-04-21 23:24:22 -0400

    fergferf

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
 import { useSelector, useDispatch } from 'react-redux';
-import { increment } from './actions';
+import { increment, decrement } from './actions';
+
+import Button from 'react-bootstrap/Button';
 
 function App() {
   const counter = useSelector(state => state.counter);
@@ -11,9 +13,18 @@ function App() {
   return (
     <div className="App">
       <h1>Counter: {counter}</h1>
-      <button onClick={() => dispatch(increment())}>+</button>
-      <button>-</button>
+      <Button onClick={() => dispatch(decrement())}>-</Button>
+      <button onClick={() => dispatch(increment(5))}>+</button>
       {isLogged ? <h3>please sign in!</h3>  : ''}
+      <>
+  <Button variant="primary">Primary</Button>{' '}
+  <Button variant="secondary">Secondary</Button>{' '}
+  <Button variant="success">Success</Button>{' '}
+  <Button variant="warning">Warning</Button>{' '}
+  <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
+  <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
+  <Button variant="link">Link</Button>
+</>
     </div>
   );
 }
--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -1,9 +1,11 @@
-const counterReduser = (state = 0, action) => {
+const counterReducer = (state = 0, action) => {
   switch(action.type){
     case 'INCREMENT':
-      return state + 1;
+      return state + action.payload;
     case 'DECREMENT':
       return state - 1;
+    default:
+      return state;
   }
 }
-export default counterReduser;
+export default counterReducer;
