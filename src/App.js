3505d5af63d5d98aff27da4a477834ba43eb78c5
tamierlan <hudojnik.1993@gmail.com>
2021-04-21 21:54:26 -0400

    third commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './App.css';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment } from './actions';
 
 function App() {
   const counter = useSelector(state => state.counter);
   const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
 
   return (
     <div className="App">
       <h1>Counter: {counter}</h1>
-      <button>+</button>
+      <button onClick={() => dispatch(increment())}>+</button>
       <button>-</button>
-      {isLogged ? <h3>please sign in!</h3>}
+      {isLogged ? <h3>please sign in!</h3>  : ''}
     </div>
   );
 }
