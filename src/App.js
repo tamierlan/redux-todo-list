625238b877fcf23c69f6c7f0220ae3b990c86f70
tamierlan <hudojnik.1993@gmail.com>
2021-04-24 22:49:10 -0400

    commits

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+
 import { useSelector, useDispatch } from 'react-redux';
 import { increment, decrement } from './actions';
 
-import Button from 'react-bootstrap/Button';
 
 function App() {
   const counter = useSelector(state => state.counter);
@@ -13,20 +16,72 @@ function App() {
   return (
     <div className="App">
       <h1>Counter: {counter}</h1>
-      <Button onClick={() => dispatch(decrement())}>-</Button>
-      <button onClick={() => dispatch(increment(5))}>+</button>
+      <Button className='m-3' onClick={() => dispatch(decrement())}>-</Button>
+      <Button className='m-3' onClick={() => dispatch(increment(5))}>+</Button>
       {isLogged ? <h3>please sign in!</h3>  : ''}
-      <>
-  <Button variant="primary">Primary</Button>{' '}
-  <Button variant="secondary">Secondary</Button>{' '}
-  <Button variant="success">Success</Button>{' '}
-  <Button variant="warning">Warning</Button>{' '}
-  <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
-  <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
-  <Button variant="link">Link</Button>
-</>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+//
+// import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+//
+// import { Container, Row, Col, Button, Alert, Breadcrumb, Card, Form } from 'react-bootstrap';
+//
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <Container>
+//           <Form>
+//             <Row>
+//               <Col>
+//                 <Form.Group controlId='formEmail'>
+//                   <Form.Label>Email Address</Form.Label>
+//                   <Form.Control type='email' placeholder='Example@gmail.com' />
+//                   <Form.Text className='text-muted'>
+//                     We will never share your email address, trust us!
+//                   </Form.Text>
+//                 </Form.Group>
+//               </Col>
+//               <Col>
+//                 <Form.Group controlId='formPassword'>
+//                   <Form.Label>Password</Form.Label>
+//                   <Form.Control type='password' placeholder='Password' />
+//                 </Form.Group>
+//               </Col>
+//             </Row>
+//             <Button className='mb-4' variant='secondary' type='submit'>Login</Button>
+//           </Form>
+//           <Card className='mb-3' style={{color: '#000'}}>
+//             <Card.Img src='https://vistapointe.net/images/card-wallpaper-15.jpg' />
+//             <Card.Body>
+//               <Card.Title>
+//                 Card Example
+//               </Card.Title>
+//               <Card.Text>
+//                 this is the exemple of bootstrap card
+//               </Card.Text>
+//               <Button variant='primary'>Read More</Button>
+//             </Card.Body>
+//           </Card>
+//           <Breadcrumb>
+//             <Breadcrumb.Item>Test 1</Breadcrumb.Item>
+//             <Breadcrumb.Item>Test 2</Breadcrumb.Item>
+//             <Breadcrumb.Item active>Test 3</Breadcrumb.Item>
+//           </Breadcrumb>
+//           <Alert varient='success'>Alert Button</Alert>
+//           <Button className='mb-4'>Button</Button>
+//         </Container>
+//       </header>
+//     </div>
+//   );
+// }
+//
+// export default App;
